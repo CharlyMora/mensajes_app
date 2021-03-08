590205264a05a48f514e8eef71fd98874aaee8f4
CharlyMora <morandres.carlos@gmail.com>
2021-03-07 19:56:52 -0500

    lista insresion de mensaje completa para ensayo

--- src/main/java/com/mycompany/mensajes_app/mensajesService.java
+++ src/main/java/com/mycompany/mensajes_app/mensajesService.java
@@ -5,12 +5,25 @@
  */
 package com.mycompany.mensajes_app;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Andres
  */
 public class mensajesService {
     public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escriba un mensaje lampara");
+        String entrada =sc.nextLine();
+        
+        System.out.println("tu nombre");
+        String nombre = sc.nextLine();
+        
+        Mensajes mensaje = new Mensajes();
+        mensaje.setMensaje(entrada);
+        mensaje.setAutor_mensaje(nombre);
+        MensajesDAO.crearMensajeDB(mensaje);
     
     }
     public static void listarMensajes(){
